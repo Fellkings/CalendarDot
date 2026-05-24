95ba0ecc7dc20ed9941c7e4ee44feafad098cb6e
Danila Chistiakov <96588136+Fellkings@users.noreply.github.com>
2026-05-24 21:23:40 +0300

    feat: implementation of monthly navigation

--- frontend/main_view.py
+++ frontend/main_view.py
@@ -1,4 +1,5 @@
 import flet as ft
+from frontend.components import CalendarGrid
 
 def setup_main_view(page: ft.Page):
     page.title = "CalendarDot"
@@ -10,13 +11,6 @@ def setup_main_view(page: ft.Page):
     #заголовок приложения
     header = ft.Text("CalendarDot", size=32, weight=ft.FontWeight.BOLD)
 
-    #заглушка
-    calendar_placeholder = ft.Container(
-        content=ft.Text("Здесь скоро появится сетка календаря (неделя/месяц)...", color=ft.colors.GREY),
-        alignment=ft.alignment.center,
-        expand=True,
-        bgcolor=ft.colors.GREY_100,
-        border_radius=10
-    )
+    calendar_grid = CalendarGrid()
 
-    page.add(header, calendar_placeholder)
+    page.add(header, calendar_grid)
